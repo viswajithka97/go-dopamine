650b54d5bee97cf92a9fb90a059606f796635648
Viswajith K A <111951269+ViswajithOfficial@users.noreply.github.com>
2023-05-17 16:09:24 +0530

    changed env path

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoadEnvFile() {
-	err := godotenv.Load()
+	err := godotenv.Load("/home/ec2-user/dopamine/go-dopamine/go-dopamine/.env")
 
 	if err != nil {
 		log.Fatal("Error loading env file")
